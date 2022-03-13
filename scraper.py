020941483620be7d93ea9e7fc669a3e477978815
amiteshnagarkar <amiteshnagarkar@gmail.com>
2022-03-13 15:02:38 +0000

    saves data jobs to excel doc

--- scraper.py
+++ scraper.py
@@ -19,6 +19,7 @@ import re
 
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 #url = "https://www.inspiringinterns.com/job-board/all"
 #page = urlopen(url)
@@ -35,14 +36,24 @@ while page != 50:
       soup = BeautifulSoup(html, "html.parser")
       
       
-      #only returns job titles that have Data.
+      
       for h1 in soup.find_all("h1", class_="hide-for-mobile"):
             job_titles.append(h1.get_text(strip=True))
       page = page + 1
 #print(len(job_titles))
 #print(job_titles)
 
+
 result = list(filter(lambda x: "Data" in x, job_titles))
 n=588 #use result of print(len(job_titles))
+
+data_jobs = []
 for title in result[:n]:
-    print(title)
+    #print(title)
+    #data_jobs = [title]
+    data_jobs.append(title)
+    df = pd.DataFrame(data_jobs)
+    writer = pd.ExcelWriter('InspiringInterns-DJ.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Data Jobs', index=False)
+    writer.save()
+
