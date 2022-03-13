8befa344f3bad9c4f6a6ac87f03db1d3cc4b7588
amiteshnagarkar <amiteshnagarkar@gmail.com>
2022-03-13 16:09:29 +0000

    clean up, need to add req txt file

--- scraper.py
+++ scraper.py
@@ -1,59 +1,54 @@
 
+'''
+Description: 
+'''
 from urllib.request import urlopen
 import re
-
-#gets first h1 tag, using regular exp
-#regular exp not useful for printing all titles.
-
-#pattern = "<h1(.*?)>.*?</h1>"
-#match_results = re.search(pattern, html, re.IGNORECASE)
-#title = match_results.group()
-#Remove HTML tags
-#title = re.sub("<.*?>", "", title)
-#print(title)
-
-#----------------------------------------
-
-#BeautifulSoup
-
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-#url = "https://www.inspiringinterns.com/job-board/all"
-#page = urlopen(url)
-#html = page.read().decode("utf-8")
+'''
+Function: ScrapDataJobs - Takes job title as a parameter and search for job with data in them.
+'''
+def scrapDataJobs(job_titles):
+  dataJobs = []
+  
+  filterredDataJobs = list(filter(lambda x: "Data" in x, job_titles))
+  numberOfJobs = len(job_titles);
 
+  for title in filterredDataJobs[:numberOfJobs]:
+    dataJobs.append(title)
+    df = pd.DataFrame(dataJobs)
+    excelWriter = pd.ExcelWriter('InspiringInterns-DJ.xlsx', engine='xlsxwriter')
+    df.to_excel(excelWriter, sheet_name='Data Jobs', index=False)
+    excelWriter.save()
 
-page = 1
-job_titles = []
-
-while page != 50:
-      url = f"https://www.inspiringinterns.com/job-board/all?page={page}"
+'''
+Function: scrapJobTitles - Scrapes for H1 tag on the insperinginterms jobboard wbesite
+'''
+def scrapJobTitles(totalPages):
+  print('Started.. hold on...');
+  job_titles = []
+  pageCounter = 1
+  while pageCounter != totalPages:
+      url = f"https://www.inspiringinterns.com/job-board/all?page={pageCounter}"
       response = requests.get(url)
-      html = response.content
-      soup = BeautifulSoup(html, "html.parser")
-      
-      
-      
-      for h1 in soup.find_all("h1", class_="hide-for-mobile"):
-            job_titles.append(h1.get_text(strip=True))
-      page = page + 1
-#print(len(job_titles))
-#print(job_titles)
+      htmlContent = response.content
+      soup = BeautifulSoup(htmlContent, "html.parser")
+      for h1Tag in soup.find_all("h1", class_="hide-for-mobile"):
+            job_titles.append(h1Tag.get_text(strip=True))
+      pageCounter += 1;
+  return job_titles;
 
+'''
+Main module to start scraping.
+'''
+def startScraping():
+    print('Starting scraping. Looking for Data jobs on Inspiring Interns....Please wait');
+    job_titles = scrapJobTitles(50); #50 jobs at the moment but can be scrapped for actual value
+    scrapDataJobs(job_titles);
+    print('Done!. Scraping data is available at : InspiringInterns-DJ.xlsx');
 
-result = list(filter(lambda x: "Data" in x, job_titles))
-n=588 #use result of print(len(job_titles))
-
-data_jobs = []
-for title in result[:n]:
-    #print(title)
-    #data_jobs = [title]
-    data_jobs.append(title)
-    df = pd.DataFrame(data_jobs)
-    writer = pd.ExcelWriter('InspiringInterns-DJ.xlsx', engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Data Jobs', index=False)
-    writer.save()
-
+if __name__ == "__main__":
+  startScraping();
